66a207f47c7fe6298d2d2e4d0ebc4a6e785ae155
lutian <lsalvino@hotmail.com>
2015-11-18 17:07:12 -0300

    update example

--- picStylizer/example.php
+++ picStylizer/example.php
@@ -9,7 +9,7 @@ $pS = new picStylizer();
 $pS->setMinization();
 
 // define css style by default
-$css = 'body {backgound-color:#000;font-family:courier;color:#fff,font-size:14px;}';
+$css = 'body {background-color:#000;font-family:courier;color:#fff;font-size:14px;}';
 $pS->setCssInit($css);
 
 // gen sprites, styles and html example
